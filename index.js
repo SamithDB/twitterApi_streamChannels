aa20871ba7fee68e00348736d65ad7a30b2fde25
unknown <sam@cloudpartners.biz>
2019-01-15 00:20:02 +0530

    stream channels

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var _ = require('lodash');
 
 var TwitterStreamChannels = require('twitter-stream-channels');
 
-nconf.file({ file: 'config.json' }).env(); // Twitter Consumer key & Access key
+nconf.file({ file: 'config.json' }).env(); // Twitter Consumer key & Access key  --For Twit
 
-var credentials = require('./config.json')
-var client = new TwitterStreamChannels(credentials);
+var credentials = require('./config2.json')            //For TwitterStreamChannels
+var client = new TwitterStreamChannels(credentials);  //For TwitterStreamChannels	
 
 var twitter = new Twit({
   consumer_key: nconf.get('TWITTER_CONSUMER_KEY'),
@@ -16,8 +16,14 @@ var twitter = new Twit({
   access_token_secret: nconf.get('TWITTER_ACCESS_TOKEN_SECRET')
 });
 
-// Stream a sample of public statuses ...
+var channels = {  //Filter Chanels
+	"languages" : ['javascript','php','java','python','perl'],
+	"js-frameworks" : ['angularjs','jquery','backbone','emberjs'],
+	"web" : ['javascript','nodejs','html5','css','angularjs']
+};
 /*
+//------------------------------------------------------------------------ Stream a sample of public statuses ...
+
 var tweetStream = twitter.stream('statuses/sample');
 
 tweetStream.on('tweet', function (tweet) {
@@ -27,13 +33,13 @@ tweetStream.on('tweet', function (tweet) {
   
 }); 
 
-//-----------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------ Stream ausing a filter and location ...
 
 var srilanka = [ '79.6951668639', '5.96836985923', '81.7879590189', '9.82407766361' ]
 
 //  filter the twitter public stream 
 
-var stream = twitter.stream('statuses/filter', { track:['Apple','Mango'] })
+var stream = twitter.stream('statuses/filter', { locations: srilanka , track: 'Apple' })
  
 stream.on('tweet', function (tweet) {
   console.log('screen_name:', tweet.user.screen_name);
@@ -42,6 +48,10 @@ stream.on('tweet', function (tweet) {
 })
 */
 
-//-----------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------ StreamChannels developing ...
 
+var stream = client.streamChannels({track:channels});
 
+stream.on('channels/languages',function(tweet){
+    console.log('>languages',tweet.text);  //any tweet with 'javascript','php','java','python','perl'
+});
